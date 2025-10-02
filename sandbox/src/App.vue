48ae2cecd94ec5a09b953112ac30f94d0b971b19
Abduboriy Ahmadjonov <ahmadjonovabduboriy3@gmail.com>
2025-10-03 02:13:03 +0500

    refactor: Fetching data and data life sycle

---
+++ sandbox/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+
+export default {
+  data: () => ({
+    pokedex: [1, 2, 3],
+  }),
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((res) =>
+        res.json(),
+      )
+    },
+  },
+}
+</script>
+
+<template>
+  <h1>App</h1>
+  <pre>{{ pokedex }}</pre>
+  <button @click="fetchPokemon">Fetch Pokemon</button>
+</template>
