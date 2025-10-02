ec901c37a0d16dedf226914d97172d85af9d0436
Abduboriy Ahmadjonov <ahmadjonovabduboriy3@gmail.com>
2025-10-03 02:39:33 +0500

    feature: Adding users page

---
+++ cest-la-vue/src/components/UsersPage.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data: () => ({
+    users: {},
+  }),
+  methods: {
+    async fetchUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      return await response.json();
+    },
+  },
+  created() {
+    this.fetchUsers();
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
