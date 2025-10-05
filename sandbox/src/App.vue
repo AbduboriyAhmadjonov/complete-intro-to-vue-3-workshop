697e5b266f639a54acfc5b44c431bbf8d1c31583
Abduboriy Ahmadjonov <ahmadjonovabduboriy3@gmail.com>
2025-10-05 09:40:08 +0500

    feature: Created Pokedex component and used Composition API

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,22 +1,14 @@
 <script>
-// https://pokeapi.co/api/v2/pokemon?limit=151
-
+import Pokedex from './components/Pokedex.vue'
 export default {
-  data: () => ({
-    pokedex: [1, 2, 3],
-  }),
-  methods: {
-    async fetchPokemon() {
-      this.pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((res) =>
-        res.json(),
-      )
-    },
-  },
+  components: { Pokedex },
 }
 </script>
 
 <template>
   <h1>App</h1>
-  <pre>{{ pokedex }}</pre>
-  <button @click="fetchPokemon">Fetch Pokemon</button>
+  <Suspense>
+    <Pokedex />
+    <template #fallback>Loading Pokedex...</template>
+  </Suspense>
 </template>
---
+++ sandbox/src/components/Pokedex.vue
@@ -0,0 +1,19 @@
+<script>
+// https://pokeapi.co/api/v2/pokemon?limit=151
+
+export default {
+  async setup() {
+    const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((res) =>
+      res.json(),
+    )
+    return { pokedex }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <h2>Pokedex</h2>
+    <pre>{{ pokedex }}</pre>
+  </div>
+</template>
