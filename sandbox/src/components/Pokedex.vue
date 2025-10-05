131cf48abb580de130d5e3dd503e7e09e9d852ae
Abduboriy Ahmadjonov <ahmadjonovabduboriy3@gmail.com>
2025-10-05 20:20:46 +0500

    refactor: Using ref for reactive data

--- sandbox/src/components/Pokedex.vue
+++ sandbox/src/components/Pokedex.vue
@@ -1,12 +1,19 @@
 <script>
 // https://pokeapi.co/api/v2/pokemon?limit=151
+import { ref } from 'vue'
 
 export default {
   async setup() {
+    const regionName = ref('Tashkent')
     const pokedex = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then((res) =>
       res.json(),
     )
-    return { pokedex }
+    return { pokedex, regionName }
+  },
+  methods: {
+    changeRegionName() {
+      this.regionName = 'Kanto'
+    },
   },
 }
 </script>
@@ -14,6 +21,8 @@ export default {
 <template>
   <div>
     <h2>Pokedex</h2>
+    <p>Region: {{ regionName }}</p>
+    <button @click="changeRegionName">Change Region Name</button>
     <pre>{{ pokedex }}</pre>
   </div>
 </template>
